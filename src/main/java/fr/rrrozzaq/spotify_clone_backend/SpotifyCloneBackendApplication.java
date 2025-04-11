2d0d26bc78cd15212cd95a49b56722e3081bb2f6
Abdul Rozzaq <abdrozzaq02@gmail.com>
2025-04-11 23:12:06 +0700

    first commit

---
+++ src/main/java/fr/rrrozzaq/spotify_clone_backend/SpotifyCloneBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.rrrozzaq.spotify_clone_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyCloneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyCloneBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/rrrozzaq/spotify_clone_backend/SpotifyCloneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.rrrozzaq.spotify_clone_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyCloneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
