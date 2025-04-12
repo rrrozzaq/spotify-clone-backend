a2f4edcc6bdc06249257fcc423ce8f9210ee16f1
Abdul Rozzaq <abdrozzaq02@gmail.com>
2025-04-13 00:35:04 +0700

    chore: config dotenv

--- src/main/java/fr/rrrozzaq/spotify_clone_backend/SpotifyCloneBackendApplication.java
+++ src/main/java/fr/rrrozzaq/spotify_clone_backend/SpotifyCloneBackendApplication.java
@@ -2,11 +2,18 @@ package fr.rrrozzaq.spotify_clone_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class SpotifyCloneBackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("postgres_url", dotenv.get("postgres_url"));
+		System.setProperty("postgres_db", dotenv.get("postgres_db"));
+		System.setProperty("postgres_username", dotenv.get("postgres_username"));
+		System.setProperty("postgres_password", dotenv.get("postgres_password"));
+
 		SpringApplication.run(SpotifyCloneBackendApplication.class, args);
 	}
 
