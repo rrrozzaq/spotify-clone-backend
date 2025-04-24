c719734d82d400f7c11fa46cb016010edc2c6bfe
Abdul Rozzaq <abdrozzaq02@gmail.com>
2025-04-24 21:22:13 +0700

    feat: add song mapper

--- src/main/java/fr/rrrozzaq/spotify_clone_backend/catalogcontext/application/mapper/SongMapper.java
+++ src/main/java/fr/rrrozzaq/spotify_clone_backend/catalogcontext/application/mapper/SongMapper.java
@@ -1,9 +1,38 @@
 package fr.rrrozzaq.spotify_clone_backend.catalogcontext.application.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import fr.rrrozzaq.spotify_clone_backend.catalogcontext.application.dto.ReadSongInfoDTO;
+import fr.rrrozzaq.spotify_clone_backend.catalogcontext.application.dto.SaveSongDTO;
+import fr.rrrozzaq.spotify_clone_backend.catalogcontext.application.vo.SongAuthorVO;
+import fr.rrrozzaq.spotify_clone_backend.catalogcontext.application.vo.SongTitleVO;
+import fr.rrrozzaq.spotify_clone_backend.catalogcontext.domain.Song;
 
 @Mapper(componentModel = "spring")
 public interface SongMapper {
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "publicId", ignore = true)
+    Song saveSongDTOToSong(SaveSongDTO saveSongDTO);
+
+    @Mapping(target = "favorite", ignore = true)
+    ReadSongInfoDTO songToReadSongInfoDTO(Song song);
+
+    default SongTitleVO stringToSongTitleVO(String title) {
+        return new SongTitleVO(title);
+    }
+
+    default SongAuthorVO stringToSongAuthorVO(String author) {
+        return new SongAuthorVO(author);
+    }
+
+    default String songTitleVOToString(SongTitleVO title) {
+        return title.value();
+    }
+
+    default String songAuthorVOToString(SongAuthorVO author) {
+        return author.value();
+    }
 
 }
