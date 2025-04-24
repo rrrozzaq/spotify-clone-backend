a223984357df7d3fc955fda20fd8a24e7c124136
Abdul Rozzaq <abdrozzaq02@gmail.com>
2025-04-24 21:21:54 +0700

    feat: add song content mapper

--- src/main/java/fr/rrrozzaq/spotify_clone_backend/catalogcontext/application/mapper/SongContentMapper.java
+++ src/main/java/fr/rrrozzaq/spotify_clone_backend/catalogcontext/application/mapper/SongContentMapper.java
@@ -1,8 +1,17 @@
 package fr.rrrozzaq.spotify_clone_backend.catalogcontext.application.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import fr.rrrozzaq.spotify_clone_backend.catalogcontext.application.dto.SaveSongDTO;
+import fr.rrrozzaq.spotify_clone_backend.catalogcontext.application.dto.SongContentDTO;
+import fr.rrrozzaq.spotify_clone_backend.catalogcontext.domain.SongContent;
 
 @Mapper(componentModel = "spring")
 public interface SongContentMapper {
 
-}   
+    @Mapping(source = "song.publicId", target = "publicId")
+    SongContentDTO songContentToSongContentDTO(SongContent songContent);
+
+    SongContent saveSongDTOToSong(SaveSongDTO songDTO);
+}
